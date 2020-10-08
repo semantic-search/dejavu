101c92655ed822a50cee0cb9fb61ac310f5f569d
Akshay Ashok <aks28id@gmail.com>
2020-10-08 18:15:05 +0530

    added fast api

---
+++ main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.middleware.cors import CORSMiddleware
+
+from dejavu import Dejavu
+from dejavu.logic.recognizer.file_recognizer import FileRecognizer
+
+import json
+import os
+
+CNF_FILE = "postgres.cnf.json"
+
+with open(CNF_FILE, 'r') as f:
+    config = json.load(f)
+
+# create a Dejavu instance
+djv = Dejavu(config)
+
+ 
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/index/")
+def register(file: UploadFile = File(...)):
+    file_name = file.filename
+    with open(file_name, 'wb') as f:
+        f.write(file.file.read())
+    djv.fingerprint_file(file_name)
+    os.remove(file_name)
+
+
+@app.post("/find/")
+def post(file: UploadFile = File(...)):
+    file_name = file.filename
+    with open(file_name, 'wb') as f:
+        f.write(file.file.read())
+    results = djv.recognize(FileRecognizer, file_name)
+    os.remove(file_name)
+    return results
+
+
+ 
